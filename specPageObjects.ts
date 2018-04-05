0955ad59b6401a4b8fa8063bbffb23391f47a574
doikenichi <doi.san@gmail.com>
2018-04-04 21:20:30 -0600

    some message

---
+++ specPageObjects.ts
@@ -0,0 +1,22 @@
+// local import of the exported AngularPage class
+import {AngularHomepage} from './angularPage';
+import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
+
+// The jasmine typings are brought in via DefinitelyTyped ambient typings.
+describe('angularjs homepage', () => {
+  it('should greet the named user', () => {
+    let angularHomepage = new AngularHomepage();
+    angularHomepage.get();
+    angularHomepage.setName('Julie');
+    browser.sleep(5000);
+    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+  });
+
+  it('should list todos', function() {
+    let angularHomepage = new AngularHomepage();
+    angularHomepage.get();
+    browser.sleep(5000);
+    expect(angularHomepage.todoList.count()).toEqual(2);
+    expect(angularHomepage.todoList.get(1).getText()).toEqual('build an AngularJS app');
+  });
+});
